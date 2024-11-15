64f6fca192fead0602dc201eb5950ae4a066ba33
manef ben mansour <manef639@gmail.com>
2024-11-15 21:50:01 +0100

    restricting rainbow directive to be only on input type text

--- src/app/directives/rainbow-text.directive.ts
+++ src/app/directives/rainbow-text.directive.ts
@@ -1,7 +1,7 @@
 import { Directive, HostBinding, HostListener } from '@angular/core';
 
 @Directive({
-  selector: 'input[rainbowText]',
+  selector: 'input[type="text"][rainbowText]',
   standalone: true
 })
 export class RainbowTextDirective {
