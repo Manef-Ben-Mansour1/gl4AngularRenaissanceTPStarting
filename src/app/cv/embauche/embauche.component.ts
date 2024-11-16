74d8402c17d6ab08a4fca9e56f78164b2018debe
emna-dammak <emna.dammak@insat.ucar.tn>
2024-11-16 10:30:43 +0100

    added signal

--- src/app/cv/embauche/embauche.component.ts
+++ src/app/cv/embauche/embauche.component.ts
@@ -1,26 +1,24 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, Signal } from '@angular/core';
 import { EmbaucheService } from '../services/embauche.service';
 import { Cv } from '../model/cv';
 
 import { ItemComponent } from '../item/item.component';
 
 @Component({
-    selector: 'app-embauche',
-    templateUrl: './embauche.component.html',
-    styleUrls: ['./embauche.component.css'],
-    standalone: true,
-    imports: [
-    ItemComponent
-],
+  selector: 'app-embauche',
+  templateUrl: './embauche.component.html',
+  styleUrls: ['./embauche.component.css'],
+  standalone: true,
+  imports: [ItemComponent],
 })
 export class EmbaucheComponent {
   private embaucheService = inject(EmbaucheService);
 
-  public embauchees: Cv[] = [];
+  public embauchees: Signal<Cv[]> = this.embaucheService.embauchees;
 
   /** Inserted by Angular inject() migration for backwards compatibility */
   constructor(...args: unknown[]);
   constructor() {
-    this.embauchees = this.embaucheService.getEmbauchees();
+    // this.embauchees = this.embaucheService.embauchees;
   }
 }
